56e53ab4a697962c2896e3abb211f9d0f86469c3
AvivAbramovich <AvivAbramovich@gmail.com>
2015-07-07 20:11:13 +0300

    Hash

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
-	var openTab = localStorage.getItem('openTab');
-	if(openTab ==null)
-		openTab = 1;
-	var tabSites = JSON.parse(localStorage.getItem('tabSites'));
-	if(tabSites == null){
-		tabSites = ['[]','','[]',''];	//JSONs array
-		localStorage.setItem('tabSites', JSON.stringify(tabSites));
-	}
-
 	//Properties
+
+	var openTab;	//the current open tab
+	//1 - quick reports
+	//2 - my folders
+	//3 - my team folders
+	//4 - public folders
+
+	tabSites;	//the site(s) in each tab
 
 	var currentTabsSite;	//the site(s) of the current open tab
 
@@ -43,6 +42,8 @@ $(document).ready(function(){
 
 		//keyboard keys
 		const ESCAPE_KEY = 27;
+
+		const tabs = ["quick-reports", "my-folders", "my-team-folders", "public-folders"];
 	//consts END
 
 	//methods
@@ -118,6 +119,8 @@ $(document).ready(function(){
 			buttons.openInNewTab.show();
 			buttons.settings.removeClass('active');
 		}
+
+		window.location.hash = tabs[tab-1];
 	}
 
 	function validateURL(textval) 
@@ -205,6 +208,35 @@ $(document).ready(function(){
 	//methods END
 
 	//OnStart:
+	var hash = window.location.hash;	//check if user entered a url with anchor
+	openTab = 0;
+	if(hash.length > 0){
+		hash = hash.substr(1);	//remove the '#'
+		if(hash === tabs[0])
+			openTab = 1;
+		else{
+			if(hash === tabs[1])
+				openTab = 2;
+			else{
+				if(hash === tabs[2])
+					openTab = 3
+				else{
+					if(hash === tabs[3])
+						openTab = 4;
+				}
+			}
+		}
+	}
+
+	if(openTab == 0)
+		openTab = localStorage.getItem('openTab');
+	if(openTab ==null)	//in case it's the first time in the site and 'openTab' is not yet stored in the localStorage
+		openTab = 1;
+	var tabSites = JSON.parse(localStorage.getItem('tabSites'));
+	if(tabSites == null){
+		tabSites = ['[]','','[]',''];	//JSONs array
+		localStorage.setItem('tabSites', JSON.stringify(tabSites));
+	}
 
 	//initialize settingsFormInputs
 	settingsFormInputs = [];
