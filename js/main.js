291e00f486d6e596f769f03d6179bf54eeb37d04
AvivAbramovich <AvivAbramovich@gmail.com>
2015-07-21 11:44:22 +0300

    accessability

--- js/main.js
+++ js/main.js
@@ -406,6 +406,22 @@ $(document).ready(function(){
 
 	//Events end
 
+	//Accessability
+	$("li[role='tab']").click(function(){  
+		$("li[role='tab']").attr("aria-selected","false"); //deselect all the tabs 
+	 	$(this).attr("aria-selected","true");  // select this tab
+		var tabpanid= $(this).attr("aria-controls"); //find out what tab panel this tab controls  
+	   	var tabpan = $("#"+tabpanid);  
+		$("div[role='tabpanel']").attr("aria-hidden","true"); //hide all the panels 
+		tabpan.attr("aria-hidden","false");  // show our panel
+	 });
+
+	$("li[role='tab']").keydown(function(ev) {
+		if (ev.which ==13) {
+			$(this).click()
+		}
+	});
+
 	//calling onStart
 	onStart();
 });
